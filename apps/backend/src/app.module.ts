7b51fc1f6f5030839573a9360c3f9b20442226ea
Daniel Jancar <danieljancar@bluewin.ch>
2025-04-11 10:39:19 +0200

    chore: add simple backend config

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGO_URI'),
+                appName: configService.get<string>('MONGO_APP_NAME'),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 3000)
-    Logger.log(
-        `Server running on http://localhost:${process.env.PORT ?? 3000}`,
-        'Bootstrap'
-    )
+    const configService = app.get(ConfigService)
+    const port = configService.get<number>('PORT') || 3000
+
+    await app.listen(port)
+    Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap')
 }
 
 bootstrap()
