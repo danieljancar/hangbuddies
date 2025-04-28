4e533c11b6511154347e29e2322f7f322fef4830
Daniel Jancar <danieljancar@bluewin.ch>
2025-04-28 06:50:50 +0200

    refactor: improve sidenav layout

--- apps/frontend/src/app/shared/navigation/footer/footer.component.ts
+++ apps/frontend/src/app/shared/navigation/footer/footer.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core'
+import { MatToolbar } from '@angular/material/toolbar'
+import { NgOptimizedImage } from '@angular/common'
+import { APP_ROUTES } from '../../../common/routes'
+import { RouterLink, RouterLinkActive } from '@angular/router'
+import { MatAnchor } from '@angular/material/button'
 
 @Component({
     selector: 'app-footer',
-    imports: [],
+    imports: [
+        MatToolbar,
+        NgOptimizedImage,
+        RouterLink,
+        MatAnchor,
+        RouterLinkActive,
+    ],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
     standalone: true,
 })
-export class FooterComponent {}
+export class FooterComponent {
+    protected readonly APP_ROUTES = APP_ROUTES
+}
