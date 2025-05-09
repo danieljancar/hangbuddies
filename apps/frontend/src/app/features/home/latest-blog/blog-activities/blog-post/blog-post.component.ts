9331b8da4a9f6bfc5b7ec58cca94da378931aa42
Netshlife <levinzimmermann06@gmail.com>
2025-05-09 10:53:41 +0200

    refactor(blog-post): changed latest blog-post from custom html to mat-card

--- apps/frontend/src/app/features/home/latest-blog/blog-activities/blog-post/blog-post.component.ts
+++ apps/frontend/src/app/features/home/latest-blog/blog-activities/blog-post/blog-post.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input } from '@angular/core'
-import { PostType } from '../../../../../types/post.type'
+import { BlogPostType } from '../../../../../types/blog-post .type'
 import { DatePipe } from '@angular/common'
 import { RouterLink } from '@angular/router'
 import { APP_ROUTES } from '../../../../../common/routes'
+import {
+    MatCard,
+    MatCardAvatar,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+} from '@angular/material/card'
+import { MatIcon } from '@angular/material/icon'
 
 @Component({
     selector: 'app-blog-post',
-    imports: [DatePipe, RouterLink],
+    imports: [
+        DatePipe,
+        RouterLink,
+        MatCard,
+        MatCardAvatar,
+        MatCardContent,
+        MatCardHeader,
+        MatCardTitle,
+        MatIcon,
+    ],
     templateUrl: './blog-post.component.html',
     standalone: true,
     styleUrl: './blog-post.component.scss',
 })
 export class BlogPostComponent {
-    @Input() post: PostType = {} as PostType
+    @Input() post: BlogPostType = {} as BlogPostType
     protected readonly APP_ROUTES = APP_ROUTES
 }
