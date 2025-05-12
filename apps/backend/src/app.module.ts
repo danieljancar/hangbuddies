bf98774d1cbe7d443c375d37dc2a8c7a80b23bb0
Netshlife <111433435+RikoxCode@users.noreply.github.com>
2025-05-12 21:09:44 +0200

    fix(backend): change route name from /blog/ to /blog-post/

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { SurveyModule } from './modules/survey/survey.module'
-import { BlogPostModule } from './modules/blog/blog-post.module'
+import { BlogPostModule } from './modules/blog-post/blog-post.module'
 
 @Module({
     imports: [
@@ -22,7 +22,6 @@ import { BlogPostModule } from './modules/blog/blog-post.module'
         }),
 
         SurveyModule,
-
         BlogPostModule,
     ],
     controllers: [AppController],
