745ec902a254b2e1d40090a9f6592c91b5acaa74
Daniel Jancar <danieljancar@bluewin.ch>
2025-05-16 11:48:24 +0200

    fix: refactor comparison-cta styles

--- apps/frontend/src/app/features/home/comparison/comparison-cta/comparison-cta.component.ts
+++ apps/frontend/src/app/features/home/comparison/comparison-cta/comparison-cta.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { MatAnchor, MatButton } from '@angular/material/button'
+import { MatAnchor } from '@angular/material/button'
 import { ScrollToDirective } from '../../../../shared/directives'
 import { MatIcon } from '@angular/material/icon'
 import { APP_ROUTES } from '../../../../common/routes'
@@ -7,7 +7,7 @@ import { RouterLink } from '@angular/router'
 
 @Component({
     selector: 'app-comparison-cta',
-    imports: [MatButton, ScrollToDirective, MatAnchor, MatIcon, RouterLink],
+    imports: [ScrollToDirective, MatAnchor, MatIcon, RouterLink],
     templateUrl: './comparison-cta.component.html',
     styleUrl: './comparison-cta.component.scss',
     standalone: true,
