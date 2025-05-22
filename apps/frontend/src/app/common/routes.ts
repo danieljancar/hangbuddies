14f73fd3ec16ad88697fd84412485989f86294e5
Levin Zimmermann <111433435+RikoxCode@users.noreply.github.com>
2025-05-22 23:59:01 +0200

    fix: improve mobile version for latest-blog and blog-overview
    Co-authored-by: Daniel Jancar <112062588+danieljancar@users.noreply.github.com>

--- apps/frontend/src/app/common/routes.ts
+++ apps/frontend/src/app/common/routes.ts
@@ -30,8 +30,8 @@ export const APP_ROUTES = {
     },
     BLOG: {
         OVERVIEW: BASE_ROUTES.BLOG,
-        DETAIL: (slug: string) => `${BASE_ROUTES.BLOG}/${slug}`,
-        TAG: (tag: string) => `${BASE_ROUTES.BLOG}?t=${tag}`,
+        DETAIL: (slug: string) => `/${BASE_ROUTES.BLOG}/${slug}`,
+        TAG: (tag: string) => `/${BASE_ROUTES.BLOG}?t=${tag}`,
     },
     ABOUT: {
         LANDING: BASE_ROUTES.ABOUT,
