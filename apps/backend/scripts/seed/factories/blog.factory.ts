bea4219006d4a4823abba58a4630a1cb8e454bfd
Manuel Wicki <manu2107el@gmail.com>
2025-06-04 21:13:50 +0000

    chore: test workflow operation

--- apps/backend/scripts/seed/factories/blog.factory.ts
+++ apps/backend/scripts/seed/factories/blog.factory.ts
@@ -18,6 +18,7 @@ export async function createBlog(
                         'tag2',
                         'tag3',
                         'tag4',
+                        'tag4',
                     ]),
                 ],
                 author: faker.person.fullName(),
