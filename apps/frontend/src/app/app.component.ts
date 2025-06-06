220c2169438ebeffcae71a78f758d3f960b8c614
Nelio Gautschi <nelio.gautschi@komaxgroup.com>
2025-06-06 10:26:11 +0200

    refactor: clean up previous mistakes and unnecessary things

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
 import { SeoService } from './utils/seo.service'
 import { filter, map, mergeMap } from 'rxjs'
 import { NavbarComponent } from './shared/navigation/navbar/navbar.component'
-import { ViewSurveyPageComponent } from './features/home/view-survey/view-survey-page.component'
 
 @Component({
     selector: 'app-root',
-    imports: [NavbarComponent, ViewSurveyPageComponent],
+    imports: [NavbarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     standalone: true,
