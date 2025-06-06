b2749a489c327ef0b4b3fded4b76515e4c349792
Daniel Jancar <112062588+danieljancar@users.noreply.github.com>
2025-06-06 10:21:00 +0200

    feat(frontend): send device id header

--- apps/frontend/src/app/common/storage.constants.ts
+++ apps/frontend/src/app/common/storage.constants.ts
@@ -4,11 +4,14 @@ export const LOCAL_STORAGE_KEYS = {
     UI_STATE: {
         HIDE_LANDING: 'hide_landing',
     },
+    DEVICE: {
+        ID: 'device_id',
+    },
 } as const
 
 export type FlatLocalStorageKey =
-    (typeof LOCAL_STORAGE_KEYS.UI_STATE)[keyof typeof LOCAL_STORAGE_KEYS.UI_STATE]
-
+    | (typeof LOCAL_STORAGE_KEYS.UI_STATE)[keyof typeof LOCAL_STORAGE_KEYS.UI_STATE]
+    | (typeof LOCAL_STORAGE_KEYS.DEVICE)[keyof typeof LOCAL_STORAGE_KEYS.DEVICE]
 export interface ExpiringLocalStorageValue<T> {
     value: T
     expiresAt: number // timestamp
